3c61bf5fbb817b90145ecd6b50ae25879c527eef
suryaDon-1 <suryanshguleria447@gmail.com>
2026-05-29 17:08:09 +0530

    Update API base URL to include '/api' path

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "https://aiquestion-iart.onrender.com",
+    baseURL: "https://aiquestion-iart.onrender.com/api",
     withCredentials: true
 });
 export default api;
