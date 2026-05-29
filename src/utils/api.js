4fef92f9b826b347d16f9fcc1f9f1761c46ef70f
suryaDon-1 <suryanshguleria447@gmail.com>
2026-05-29 16:46:39 +0530

    Change API base URL to production endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://aiquestion-iart.onrender.com",
     withCredentials: true
 });
 export default api;
